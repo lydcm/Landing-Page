6f59043a40e96530eed12f424be2f12df1b58f5a
Lydia Chandrahirawati <lydia.c.mink@gmail.com>
2020-05-24 06:42:34 -0700

    Final Version

--- js/app.js
+++ js/app.js
@@ -1,57 +1,82 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
-/**
- * Define Global Variables
- * 
-*/
+// Global Variables
+const navbarSections = document.querySelectorAll('section')
+const navbarList = document.getElementById('navbar__list')
 
+// Function for building navigation bar
+function buildNavbar() {
+    for (let listItem of navbarSections) {
+        let section = document.createElement('li');
+        section.className = 'menu__link';
+        section.dataset.nav = listItem.id;
+        section.innerText = listItem.dataset.nav;
+        navbarList.appendChild(section);
+    }
+};
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// Function for checking which section is currently active
+function getActiveElement() {
+    topSection = navbarSections[0];
+    minVal = 1000000;
+    for (listItem of navbarSections) {
+        let rectangle = listItem.getBoundingClientRect();
+        if (rectangle.top > -300 & rectangle.top < minVal) {
+            minVal = rectangle.top;
+            topSection = listItem;
+        };
+    };
+    return topSection;
+};
 
+// Function for adding class 'active' to section when near top of viewport
+function setActiveToSection () {
+    window.addEventListener('scroll', function (event) {
+        let section = getActiveElement();
+        section.classList.add('active-class');
+        for (let listItem of navbarSections) {
+            if (listItem.id != section.id & listItem.classList.contains('active-class')) {
+                listItem.classList.remove('active-class');
+            }
+        }
+        const activeElement = document.querySelector('li[data-nav="' + section.id + '"]');
+        activeElement.classList.add('active__link');
+        const headers = document.querySelectorAll('.menu__link');
+        for (let item of headers) {
+            if (item.dataset.nav != activeElement.dataset.nav & item.classList.contains('active__link')) {
+                item.classList.remove('active__link');
+            }
+        };
+    });
+};
 
+// Function to scroll to clicked section
+function scrollToClickedSection() {
+    navbarList.addEventListener('click', function (event) {
+        const clickedSection = document.querySelector('#' + event.target.dataset.nav)
+        clickedSection.scrollIntoView({block: 'start', behavior: 'smooth'});
+    });
+};
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// Build menu
+buildNavbar();
 
-// build the nav
+// Scroll to section on link
+scrollToClickedSection();
 
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+// Set section as active
+setActiveToSection();
 
